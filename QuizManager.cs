8ecb10acee3e6c89d2548fd10529da62524fecd4
jomari0724 <jomjom072499@gmail.com>
2022-01-27 23:35:14 +0800

    C# Game lang lods

---
+++ QuizManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public GameObject QuizPanel;
+    public GameObject GoPanel;
+
+    public Text QuestionTxt;
+    public Text ScoreText;
+
+    int totalQuestions = 0;
+    public int score;
+
+
+    private void Start()
+    {
+        totalQuestions = QnA.Count;
+        GoPanel.SetActive(false);
+        generateQuestion();
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver()
+    {
+        QuizPanel.SetActive(false);
+        GoPanel.SetActive(true);
+        ScoreText.text = score + "/" + totalQuestions;
+    }
+
+    public void correct()
+    {
+        score += 1;
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    public void wrong()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    void generateQuestion()
+    {
+        if (QnA.Count > 0)
+        {
+        currentQuestion = Random.Range(0, QnA.Count);
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        SetAnswers();
+        }
+        else
+        {
+            Debug.Log("Out of Questions");
+            GameOver();
+        }
+      
+
+    }    
+}
